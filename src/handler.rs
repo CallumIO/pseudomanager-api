ada45f585b0c0623054185e1456b9e75c6847f0e
Callum Leslie <github@cleslie.uk>
2022-04-20 17:27:06 +0100

    Add route for post requests to API

--- src/handler.rs
+++ src/handler.rs
@@ -1,3 +1,11 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use serde::{Serialize, Deserialize}
 use serde_json::{Result, Value};
+
+#[post("/pseudonym")]
+pub async fn handle_request(payload: web::Json<serde_json::Value>)) -> impl Responder {
+    // payload is the JSON body object
+
+    // Response
+    // HttpResponse::Ok().json(Pseudonym {})
+}
