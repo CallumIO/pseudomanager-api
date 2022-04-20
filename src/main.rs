ad76779b73f745bdd0c5243b76920a9f91b3f4aa
Callum Leslie <github@cleslie.uk>
2022-04-20 16:17:35 +0100

    Create actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 #[macro_use]
 extern crate actix_web;
 use actix_web::{App, HttpServer, web};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/api")
+                    // add service handler here .service(module::function)
+            )
+    })
+    .workers(10)
+    .keep_alive(15)
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+}
